42b20219199ede83821284ad41150e3dcb84b9db
Aminu <aminuu111@gmail.com>
2022-10-18 17:16:31 -0700

    0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-#i fndef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 void print_alphabet(void);
@@ -9,9 +9,9 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void times_ table(void);
+void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 int _putchar(char c);
 
-#endif
+# endif
